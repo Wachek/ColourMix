736f4a289db49c545c5136650a099c88633efe34
Viacheslav Turchak <wachek7@gmail.com>
2022-06-13 20:45:46 +0300

    V2 Setup user interface

---
+++ ColourMix/ViewControllers/ResultViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ResultViewController.swift
+//  ColourMix
+//
+//  Created by Вячеслав Турчак on 12.06.2022.
+//
+
+import UIKit
+
+class ResultViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+    
+
+
+}
